41b88b6e930030129872aacd7b233c3d0f227c41
LeiamNash <94122613+LeiamNashRebirth@users.noreply.github.com>
2023-12-10 12:03:17 +0800

    Create prefix.js

---
+++ 2.0.1/prefix.js
@@ -0,0 +1,13 @@
+exports.leiamnash = async function({ alice, api, axios, bot, cache, database, event, font, fs, language, log, message, path, scraper, wrapper }) {
+try {
+ const leiamA = [ "prefix", "prefex", "prifex", "prifix" ]
+ const leiamW = [ "❤️", "🧡", "💛", "💚", "💙", "🤎", "🖤", "🤍" ]
+leiamA.forEach(leiam => {
+if (event.body.toLowerCase().startsWith(leiam)) {
+(async function() {
+  const leiamY = (await bot.getUserInfo(event.senderID))[event.senderID].name;
+  return message(await font (`prefix: ${alice.prefix}\nplease don't forget my prefix honey ${leiamY}\n\nuse ⟨ ${alice.prefix}help ⟩ for commands list`), `${leiamW[Math.floor(Math.random() * leiamW.length)]}`);
+    })();
+   }
+  });
+} catch (error) {}
